0785c02b8f11009967a4c53c6833ede777d464b4
Akshay <akshaykm1100@gmail.com>
2024-05-08 22:36:03 +0530

    added static_root to settings.py

--- SunderKart/SunderKart/settings.py
+++ SunderKart/SunderKart/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
